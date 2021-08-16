80ec003ebdf9268bdecae38f117963d94b37367f
hmmvanminhhh <vanminh14817646@gmail.com>
2021-08-16 21:45:46 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'He Lu chào Huyền nhé!',
+    introDesc: 'Anh có điều này muốn hỏi Thêm nhớ phải trả lời thật lòng nhaaa',
+    btnIntro: 'Ô Cê Anh',
+    title: 'Có thích anh đúng hông nào😊',
+    desc: 'Ko trả lời mà thoát ra tức là yêu anh nhá😁',
+    btnYes: 'Thích rất rất nhiềuuu🥰🥰🥰',
+    btnNo: 'Anh mơ à :3',
+    question:'Lí do thích Anh là gi nèe <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vì Anh đẹp try vllll😍',
+    mess: 'Anh biết màa^^ Yêu em😘😘',
+    messDesc: 'Nhớ giữ gìn sức khoẻ ăn uống đầy đủ đúng giờ giấc nhe. Hông là trở bệnh á ở 1 mình hông ai chăm đc đâu. Hihi ❤',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/beatboxer.minh/' 
+}
